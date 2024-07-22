9d54f8a73e791b979f3e920c5ecfa3bea31227e6
Yaw Ansong Snr <yaw.srr@gmail.com>
2024-07-22 21:30:39 +0100

    updated version of package in CHANGELOG and README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyautolabnova",
-    version="0.0.1a2",
+    version="0.0.1a3",
     author="Dr. Yaw Ofosu Ansong Snr",
     author_email="yaw.srr@gmail.com",
     description="A python package for interfacing with Metrohm Autolab instruments",
